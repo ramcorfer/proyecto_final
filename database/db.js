505c729dec7ed3bd83aaea66cd3168af27a6c847
Ramiro Cornejo <rcornejo@emergya.com>
2023-06-12 10:21:49 +0200

    cambio de de port a variable de entorno

--- database/db.js
+++ database/db.js
@@ -1,10 +1,13 @@
 const mysql = require('mysql');
 
+
+
 const conexion = mysql.createConnection({
     host : process.env.DB_HOST,
     user : process.env.DB_USER,
     password : process.env.DB_PASS,
     database : process.env.DB_DATABASE,
+    PORT: process.env.DBPORT
 });
 
 conexion.connect((error) => {
